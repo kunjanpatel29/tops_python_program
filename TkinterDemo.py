bd894323400255cd49ce79da96b8530290559aa7
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-03-28 22:53:13 +0530

    28-3-2023

--- TkinterDemo.py
+++ TkinterDemo.py
@@ -30,6 +30,50 @@ def insert_data():
         e_mobile.delete(0,'end')
         msg.showinfo("Insert Status","Data Inserted Successfully")
 
+# Search Data By ID
+def search_data():
+    e_fname.delete(0,'end')    # To Delete Data from TextField only
+    e_lname.delete(0,'end')
+    e_email.delete(0,'end')
+    e_mobile.delete(0,'end')
+    if e_id.get()=="":
+        msg.showinfo("Search Status","Id is Mandatory For Serach")
+    else:
+        conn=create_conn()
+        cursor=conn.cursor()
+        query="select * from student where id=%s"
+        args=(e_id.get(),)
+        cursor.execute(query,args)
+        row=cursor.fetchall()
+        if row:
+            for i in row:
+                e_fname.insert(0,i[1])
+                e_lname.insert(1,i[2])
+                e_email.insert(2,i[3])
+                e_mobile.insert(3,i[4])
+        else:
+            msg.showinfo("Serach Status","Id Not Found")
+        conn.close()
+
+# Update Data
+def update_data():
+    if e_fname.get()=="" or e_lname.get()=="" or e_email.get()=="" or e_mobile.get()=="" or e_id.get()=="":
+        msg.showmsg("Updata Status","All Fields are Mandatory")
+    else:
+        conn=create_conn()
+        cursor=conn.cursor()
+        query="update student set fname=%s,lname=%s,email=%s,mobile=%s where id=%s"
+        args=(e_fname.get(),e_lname.get(),e_email.get(),e_mobile.get(),e_id.get())
+        cursor.execute(query,args)
+        conn.commit()
+        conn.close()
+        e_fname.delete(0,'end')    # To Delete Data from TextField only
+        e_lname.delete(0,'end')
+        e_email.delete(0,'end')
+        e_mobile.delete(0,'end')
+        e_id.delete(0,'end')
+        msg.showinfo("Update Status","Data Updated Successfully")
+        
 
 root = Tk()                 # object of Tk Class
 root.geometry("400x380")    # It will Open Page in size of 400x380
@@ -72,10 +116,10 @@ e_mobile.place(x=200,y=250)
 insert=Button(root,text="INSERT",font=("Arial",10),fg="white",bg="black",command=insert_data)
 insert.place(x=50,y=300)
 
-search=Button(root,text="SEARCH",font=("Arial",10),fg="white",bg="black")
+search=Button(root,text="SEARCH",font=("Arial",10),fg="white",bg="black",command=search_data)
 search.place(x=110,y=300)
 
-update=Button(root,text="UPDATE",font=("Arial",10),fg="white",bg="black")
+update=Button(root,text="UPDATE",font=("Arial",10),fg="white",bg="black",command=update_data)
 update.place(x=178,y=300)
 
 delete = Button(root,text="DELETE",font=("Arial",10),fg="white",bg="black")
