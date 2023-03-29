6eac17368af0b89830b1d54049593e361ed62483
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-03-29 18:08:26 +0530

    29-3-2023

---
+++ ExceptionDemo.py
@@ -0,0 +1,38 @@
+"""
+Exception : It is an abnormal condition that occurs during the Runtime of a program.
+
+Exception Keyword : Try, Except & Finally
+try : The try keyword is used to specify a block where we should place an exception code.
+except : The except block is used to handle the exception if try block raises an error.
+finally : The finally block will Always be Executed if the try block raises an error or not.
+"""
+
+print("Start Code")
+
+try:
+    a=int(input("Enter A : "))
+    b=int(input("Enter B : "))
+
+    c=a/b
+
+    print("Division : ",c)
+    l=[1,2,3,4,5]
+    index = int(input("Enter a Index Number : "))
+    print(l[index])
+
+except Exception as e:
+    print("Exception Caught : ",e)
+
+finally:
+    print("I am Finally Block Always Be Executed")
+
+# Inbuilt Exception Classes or Types of Exceptions
+""" except ZeroDivisionError as e:
+    print("Exception Caught : ",e)
+except ValueError as e:
+    print("Exception Caught : ",e)
+except IndexError as e:
+    print("Exception Caught : ",e) """
+
+print("End Code")
+
