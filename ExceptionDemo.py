5c856e05ff794e775bd9ed4cb51860ef61cba058
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-03-29 22:52:54 +0530

    29-3-2023

--- ExceptionDemo.py
+++ ExceptionDemo.py
@@ -1,7 +1,7 @@
 """
 Exception : It is an abnormal condition that occurs during the Runtime of a program.
 
-Exception Keyword : Try, Except & Finally
+Exception Handling Keyword : Try, Except & Finally
 try : The try keyword is used to specify a block where we should place an exception code.
 except : The except block is used to handle the exception if try block raises an error.
 finally : The finally block will Always be Executed if the try block raises an error or not.
@@ -20,7 +20,7 @@ try:
     index = int(input("Enter a Index Number : "))
     print(l[index])
 
-except Exception as e:
+except Exception as e:   # The Parent Class of All Exception Classes
     print("Exception Caught : ",e)
 
 finally:
