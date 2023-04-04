3c71e19284bbfc4672e7ff9722bb3bf441b43860
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-04-04 23:58:13 +0530

    Removed Files Because I Moved Files into Assignment Repository.

--- CheckNumber_Assignment.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Enter a Number: "))
-
-if n > 0:
-    print(n,"Is a Positive Number")
-elif n < 0:
-    print(n,"Is a Negative Number")
-else:
-    print("Number is a Zero")
--- CheckVowel_Assignemnt.py
+++
@@ -1,9 +0,0 @@
-# Take Input from the user
-ch = input("Enter a Character: ")
-
-# Checking for given Character is a vowel or not
-if (ch == "A" or ch == "a" or ch == "E" or ch == "e" or ch == "I" or ch == "i" or ch == "O"
-or ch == "o" or ch == "U" or ch == "u"):
-    print(ch, " Is a Vowel")
-else:
-    print(ch , " Is a Consonant")
