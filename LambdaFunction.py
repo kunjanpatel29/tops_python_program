9e01684b97b31ff53e710f4076b437e7ee3726d7
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-05-21 23:37:19 +0530

    21-05-2023

--- LambdaFunction.py
+++ LambdaFunction.py
@@ -23,8 +23,22 @@ x = lambda a,b : a * b
 print(x(9,8))
 
 # Lambda Function with List Comprehension
-
 is_even_list = [lambda arg=x: arg * 10 for x in range(1, 5)]
- 
+
 for item in is_even_list:
     print(item())
+
+# Lambda Fuction with if-else
+Max = lambda a,b : a if a > b else b
+print(Max(1,2))
+
+# Using Filter and Lambda Function
+li = [5, 7, 22, 97, 54, 62, 77, 23, 73, 61]
+final_list  = list(filter(lambda x : (x % 2 != 0),li))
+print(final_list)
+
+# Using Filter and Lambda Function
+ages = [13, 90, 17, 59, 21, 60, 5]
+adults = list(filter(lambda age: age > 18, ages))
+print(adults)
+
