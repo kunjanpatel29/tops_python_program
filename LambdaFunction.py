f8883ea8d28d8ac1f74d2e5298b27e12ddd0d97f
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-05-22 22:04:39 +0530

    22-05-2023

--- LambdaFunction.py
+++ LambdaFunction.py
@@ -46,3 +46,8 @@ print(adults)
 li = [5, 7, 22, 97, 54, 62, 77, 23, 73, 61]
 final_list = list(map(lambda x: x*2,li))
 print(final_list)
+
+# Using Map and Lambda Function
+animals = ['dog', 'cat', 'parrot', 'rabbit']
+uppered_animals = list(map(lambda animal: animal.upper(), animals))
+print(uppered_animals)
