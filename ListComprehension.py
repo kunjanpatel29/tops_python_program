4a8f4d3f36524e16a926d3a841df68152a79682d
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-05-22 22:14:14 +0530

    22-05-2023

--- ListComprehension.py
+++ ListComprehension.py
@@ -4,3 +4,9 @@
 List = [character for character in [1, 2, 3]]
 print(List)
 
+List = [character for character in range(1,30)]
+print(List)
+
+# Even list using list comprehension
+my_list = [i for i in range(1,101) if i % 2 == 0]
+print(my_list)
