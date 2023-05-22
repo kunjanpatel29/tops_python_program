ee0ccfaac376a075a55748b10dc02a0ab732b6bf
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-05-22 22:02:00 +0530

    22-05-2023

--- LambdaFunction.py
+++ LambdaFunction.py
@@ -42,3 +42,7 @@ ages = [13, 90, 17, 59, 21, 60, 5]
 adults = list(filter(lambda age: age > 18, ages))
 print(adults)
 
+# Using Map and Lambda Function
+li = [5, 7, 22, 97, 54, 62, 77, 23, 73, 61]
+final_list = list(map(lambda x: x*2,li))
+print(final_list)
