b8f77c530ca930fcc887ebe45427f5d1a8db5529
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-05-22 22:20:44 +0530

    22-05-2023

--- ListComprehension.py
+++ ListComprehension.py
@@ -10,3 +10,11 @@ print(List)
 # Even list using list comprehension
 my_list = [i for i in range(1,101) if i % 2 == 0]
 print(my_list)
+
+#Matrix using List comprehension
+matrix = [[j for j in range(3)] for i in range(3)] 
+print(matrix)
+
+List = [character for character in 'KunjanPatel']
+print(List)
+
