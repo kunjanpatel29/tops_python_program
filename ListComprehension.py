510925a87055d85a19a9489a41425fc0c7439af0
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-05-25 22:20:34 +0530

    25-05-2023

--- ListComprehension.py
+++ ListComprehension.py
@@ -37,3 +37,7 @@ lis = [num for num in range(100)
 	if num % 5 == 0 if num % 10 == 0]
 print(lis)
 
+#Display a square of numbers from 1 to 10
+squares = [n**2 for n in range(1, 11)]
+print(squares)
+
