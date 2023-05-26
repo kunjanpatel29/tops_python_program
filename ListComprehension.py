8b0f96a2758c7f374745e61abb5d1073766f3e5b
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-05-26 23:32:26 +0530

    26-05-2023

--- ListComprehension.py
+++ ListComprehension.py
@@ -68,3 +68,10 @@ print(list(result))
 numbers = (1, 2, 3, 4)
 result = map(lambda x: x + x, numbers)
 print(list(result))
+
+# Add two lists using map and lambda
+numbers1 = [1, 2, 3]
+numbers2 = [4, 5, 6]
+result = map(lambda x, y: x + y, numbers1, numbers2)
+print(list(result))
+
