d3df610b7913c1397b8601b1476ad1b79edc694e
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-05-27 23:43:07 +0530

    27-05-2023

--- SetDemo.py
+++ SetDemo.py
@@ -1,8 +1,20 @@
 """ Sets are used to store multiple items in a single variable. A set is a collection
 which is unordered, unchangeable*, and unindexed. """
 
-l = {1,2,1.1,1.2,"tops",10,320,1,2,"Python",False}
-print(l)
+s = {1,2,1.1,1.2,"tops",10,320,1,2,"Python",False}
+print(s)
 
-l.add(9)   # Add an Element to the set
-print(l)
+s.add(9)         # Add an Element to the set
+print(s)
+
+s1=s.copy()      # Copy Whole list from s to s1
+print(s1)
+
+s.pop()          # Remove a random item from the set
+print(s)
+
+s.remove(320)    # Remove a Specified item from the set
+print(s)
+
+#s.clear()        # Clear the set  
+#print(s)
