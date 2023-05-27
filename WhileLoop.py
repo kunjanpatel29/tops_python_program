5080744881c7432953cc12ad6dfeb8d983ecc748
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-05-27 22:58:59 +0530

    27-05-2023

--- WhileLoop.py
+++ WhileLoop.py
@@ -4,12 +4,3 @@ while n > 0:
     sum = sum + n 
     n = n - 1
 print("Sum is: ",sum)
-
-
-# Print i from 1 to 5 
-i=1
-while i < 6:
-    print(i)
-    i = i + 1
-    
-    
---
+++ ZipFunction.py
@@ -0,0 +1,5 @@
+# Python zip() with lists
+name = [ "Kunjan", "Sanjay", "Vikas", "Keval" ]
+roll_no = [ 4, 1, 3, 2 ]
+mapped = zip(name, roll_no)
+print(set(mapped))
