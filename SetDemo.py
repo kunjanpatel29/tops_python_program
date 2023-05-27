34d84a5b1693e0b045abdb450e957867efa1cf6a
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-05-27 23:55:36 +0530

    27-05-2023

--- SetDemo.py
+++ SetDemo.py
@@ -16,5 +16,19 @@ print(s)
 s.remove(320)    # Remove a Specified item from the set
 print(s)
 
+s.discard(320)    # Remove a Specified item from the set
+print(s)
+
 #s.clear()        # Clear the set  
 #print(s)
+
+a={1,2,3,4,5}
+b={4,5,6,7,8}
+
+c=a.union(b)
+print("Union is : ",c)
+
+c=a.intersection(b)
+print("Intersection is : ",c)
+
+
