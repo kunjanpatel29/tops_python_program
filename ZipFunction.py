f3a8aa96e26aee650079a9f1146292c55834d24e
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-05-27 23:02:45 +0530

    27-05-2023

--- ZipFunction.py
+++ ZipFunction.py
@@ -3,3 +3,10 @@ name = [ "Kunjan", "Sanjay", "Vikas", "Keval" ]
 roll_no = [ 4, 1, 3, 2 ]
 mapped = zip(name, roll_no)
 print(set(mapped))
+
+#Python zip() with enumerate
+names = ['Kunjan', 'Rohit', 'Sagar']
+ages = [23, 24, 27]
+
+for i, (name,age) in enumerate(zip(names,ages)):
+    print(i,name,age)
