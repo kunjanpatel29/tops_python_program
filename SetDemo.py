851abcf341d84e1e6985cc388ba816f4ee7f0acf
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-05-27 23:32:12 +0530

    27-05-2023

---
+++ SetDemo.py
@@ -0,0 +1,8 @@
+""" Sets are used to store multiple items in a single variable. A set is a collection
+which is unordered, unchangeable*, and unindexed. """
+
+l = {1,2,1.1,1.2,"tops",10,320,1,2,"Python",False}
+print(l)
+
+l.add(9)   # Add an Element to the set
+print(l)
