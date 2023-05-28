a68fe813cdeffbc74ef7db912f663d2257dc0fb9
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-05-28 22:59:11 +0530

    28-05-2023

--- Numpy Demo.py
+++ Numpy Demo.py
@@ -3,8 +3,23 @@ for working in domain of linear algebra, fourier transform, and matrices. It Sta
 
 import numpy as np
 
-#Creating Array object
-arr=np.array([1,2,3,4,5])
+# Creating Array object
+arr=np.array([[1,2,3],[4,5,6]])
 
-#Printing the array
+# Printing the array
 print(arr)
+
+# Printing type of arr object
+print("Array is of type: ", type(arr))
+
+# Printing array dimensions (axes)
+print("No. of dimensions: ", arr.ndim)
+
+# Printing shape of array
+print("Shape of array: ", arr.shape)
+
+# Printing size (total number of elements) of array
+print("Size of array: ", arr.size)
+
+# Printing type of elements in array
+print("Array stores elements of type: ", arr.dtype)
