275fdf00bb32be4a6efb9f11729059914659eb2b
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-05-28 22:52:54 +0530

    28-05-2023

---
+++ Numpy Demo.py
@@ -0,0 +1,10 @@
+""" Numpy : NumPy is a Python library used for working with arrays. It also has functions
+for working in domain of linear algebra, fourier transform, and matrices. It Stands For Numerical Python. """ 
+
+import numpy as np
+
+#Creating Array object
+arr=np.array([1,2,3,4,5])
+
+#Printing the array
+print(arr)
