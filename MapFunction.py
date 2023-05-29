da6dc57ab2f26222d9fbe7cbc9e49f6fe94a17a6
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-05-29 23:36:36 +0530

    29-05-2023

--- MapFunction.py
+++ MapFunction.py
@@ -17,3 +17,25 @@ print(list(result))
 l = ['sat', 'bat', 'cat', 'mat']
 test = list(map(list, l))
 print(test)
+
+# Add two lists using map and lambda
+numbers1 = [1, 2, 3]
+numbers2 = [4, 5, 6]
+result = map(lambda x, y: x + y, numbers1, numbers2)
+print(list(result))
+
+# Define a function that doubles even numbers and leaves odd numbers
+def double_even(num):
+    if num % 2 == 0:
+        return num * 2
+    else:
+        return num
+ 
+# Create a list of numbers
+numbers = [1, 2, 3, 4, 5, 6]
+ 
+# Use map to apply the function to each element in the list
+result = list(map(double_even, numbers))
+ 
+# Display the result
+print(result)  
