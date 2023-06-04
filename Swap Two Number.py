38b10beae244295a4d2980df0ae43a1164445cb7
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-04 22:38:16 +0530

    04-06-2023

--- Swap Two Number.py
+++ Swap Two Number.py
@@ -6,4 +6,13 @@ print("X : ",x,"Y : ",y)
 x,y = y,x
 print("X : ",x,"Y : ",y)
 
+# Swap Two Number Using Third Variable
 
+x,y = 10,20
+print("\nX : ",x,"Y : ",y)
+
+temp = x
+x = y
+y = temp
+
+print("X : ",x,"Y : ",y)
