c4796ac685ddc699701aa7db47a55c680821fc32
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-04 23:05:29 +0530

    04-06-2023

--- Pattern Programs.py
+++ Pattern Programs.py
@@ -13,3 +13,16 @@ for i in range(n+1,0,-1):
     print()
 
 
+# Number Pattern
+n = int(input("Enter Number of Rows : "))
+for i in range(n+1):
+    for j in range(i):
+        print(i,end=" ")
+    print()
+
+# Half pyramid pattern with the number
+n = int(input("Enter Number of Rows : "))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
