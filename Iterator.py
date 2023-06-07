1e47435cfab9e3785b796ee9f48bf1b2216801ed
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-07 22:55:34 +0530

    07-06-2023

---
+++ Iterator.py
@@ -0,0 +1,9 @@
+# Return an iterator from a tuple, and print each value:
+mytuple = ("Kunjan","Vikas","Sanjay","Prashant")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
