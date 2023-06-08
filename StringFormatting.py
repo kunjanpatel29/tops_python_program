3554bdf5a3b5600157e5d6f1fce2df87f6aa42cd
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-08 23:09:21 +0530

    08-06-2023

--- StringFormatting.py
+++ StringFormatting.py
@@ -2,3 +2,9 @@
 
 print('We all are {}.'.format('Friends'))
 print('{1} {0}'.format('Programming','Python'))
+
+print('a: {a},b: {b},c: {c}'.format(a=2,b='Python',9.2))
+print('The valueof pi is: {0:1.5f}'.format(3.141592))
+s1 = "My name is {fname}, I'm {age}".format(fname = "Kunjan", age = 23)
+print(s1)
+
