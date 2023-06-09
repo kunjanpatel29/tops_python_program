a0231ce03c3ebdbfee7d572bf73d4fd835a47653
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-09 23:38:23 +0530

    09-06-2023

---
+++ Hollow Square Pattern.py
@@ -0,0 +1,10 @@
+# Hollow Square Pattern
+
+size = int(input("Enter Number of Rows : "))
+for i in range(size):
+    for j in range(size):
+        if i == 0 or i == size - 1 or j == 0 or j == size - 1:
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print()
