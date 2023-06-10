6e7b47388a779ab8bbd21ac6a32bab65e5b73531
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-10 23:05:18 +0530

    10-06-2023

---
+++ Full Pyramid Star.py
@@ -0,0 +1,8 @@
+# Full Pyramid Pattern
+num = int(input("Enter Number of Rows : "))
+for i in range(1,num+1):
+    for j in range(num-i):
+        print(" ",end='')
+    for j in range(2*i-1):
+        print("*",end='')
+    print()
