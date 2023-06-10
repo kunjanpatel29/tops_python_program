54ff1f2e45b2a1cebbcd35c981d6b9e3115e4eb1
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-10 23:41:14 +0530

    10-06-2023

--- Half Pyramid Alphabet.py
+++ Half Pyramid Alphabet.py
@@ -1,10 +1,10 @@
 # Half pyramid using alphabets
 
 n = int(input("Enter Number of Rows : "))
-value = 65
+number = 65
 for i in range(n):
     for j in range(i+1):
-        char = chr(value)
-        print(char,end=' ')
-    value += 1
+        character = chr(number)
+        print(character,end=' ')
+    number += 1
     print()
