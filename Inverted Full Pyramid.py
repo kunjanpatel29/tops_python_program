3c926aca59fab4f95d1c5122100ba226f14f92f6
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-10 23:10:45 +0530

    10-06-2023

---
+++ Inverted Full Pyramid.py
@@ -0,0 +1,8 @@
+# Inverted Full Pyramid Pattern
+num = int(input("Enter Number of Rows : "))
+for i in range(1,num+1):
+    for j in range(i-1):
+        print(" ",end='')
+    for j in range(2*(num-i)+1):
+        print("*",end='')
+    print()
