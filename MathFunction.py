054554f77fd9f507dc7bef1459af9f2f9cf55354
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-11 23:05:41 +0530

    11-06-2023

--- MathFunction.py
+++ MathFunction.py
@@ -17,3 +17,15 @@ import math
 x = math.sqrt(81)  # Returns a Square root of a Number
 print("Square Root of a Number is : ",x)
 
+#Round a number upward to its nearest integer
+x = math.ceil(1.4)
+
+#Round a number downward to its nearest integer
+y = math.floor(1.4)
+
+print("Cell Value is : ",x)
+print("Floor Value is : ",y)
+
+# Return the Value of PI
+x = math.pi
+print("The Value of PI is : ",x) 
