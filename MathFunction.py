c28e4a73729fb7b83cbc6ae9d61c300076b48e3f
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-11 22:50:04 +0530

    11-06-2023

--- MathFunction.py
+++ MathFunction.py
@@ -5,3 +5,11 @@ y = max(10,30,25)
 
 print("Minimum Value is : ",x)
 print("Maximum Value is : ",y)
+
+x = abs(-9.25)  # returns the positive value of the specified number
+print(x)
+
+x = pow(5, 3)  # return the value of x to the power of y
+print(x)
+
+
