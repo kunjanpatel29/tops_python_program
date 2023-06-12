ea1fc7ab1043530efcc48258b9f1187814793848
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-12 23:16:37 +0530

    12-06-2023

---
+++ Operator Precedence.py
@@ -0,0 +1,23 @@
+# Operator Precedence : It can Used for order of operations.
+
+print(4+3)
+
+print(10 % 2 + 3 - 5)
+
+print(10 and 20)
+
+print(10 or 20)
+
+print(True and 30)
+
+print(3+5/2)
+
+print((3+5)/2)
+
+print(3*5/4)
+
+print(3 *(5//4))
+
+print(0 or "Hello" and 1)
+
+print((2 ** 3) ** 2)
