3bc758ab85465535625e3290af66d97743c58a7b
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-12 22:46:05 +0530

    12-06-2023

---
+++ CalculateAverageOfVariable.py
@@ -0,0 +1,23 @@
+# Nested Function and Nonlocal Keyword
+
+def calculate_average():
+
+    num1 = int(input("Enter Value of num1 : "))
+    num2 = int(input("Enter Value of num2 : "))
+    num3 = int(input("Enter Value of num2 : "))
+    average_of_variable = 0
+
+    def get_sum():
+        nonlocal average_of_variable
+        average_of_variable = num1 + num2 + num3
+        print("The Sum of Three Number is : ",average_of_variable)
+
+        def get_average():
+            average = average_of_variable / 3
+            print("The Average of Three Number is : ",average)
+
+        get_average()
+
+    get_sum()
+    
+calculate_average()
