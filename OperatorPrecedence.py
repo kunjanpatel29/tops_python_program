ccf2e3338a5722a33680ea0f3c5712a7d563f0a7
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-13 22:20:37 +0530

    13-06-2023

---
+++ OperatorPrecedence.py
@@ -0,0 +1,11 @@
+"""
+Operator Precedence:Operator precedence in Python simply refers to the order of operations.
+Operators are used to perform operations on variables and values.
+
+P - Parenthesis
+E - Exponentiation
+M - Multiplication
+D - Dicision
+A - Addition
+S - Substraction
+
