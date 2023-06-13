388d547b7c5218db5c54e253bf7e16420418eba6
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-13 22:43:20 +0530

    13-06-2023

--- OperatorPrecedence.py
+++ OperatorPrecedence.py
@@ -16,4 +16,13 @@ print(3 + 3 / 3)
 
 print((3 + 5)/2)
 
+print( ((((13+5)*2)-4)/2)-13 )
+#13+5 gives us 18
+#18*2 gives us 36
+#36-4 gives us 32
+#32/2 gives us 16.0
+#16-13 gives us 3.0
 
+print(10 % 5 / 3 - 5)
+print(10 % 2 // 3 - 5)
+print(8 / 2 + 4 - 1)
