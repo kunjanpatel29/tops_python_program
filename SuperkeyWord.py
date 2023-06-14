02e38f82ee932bfa27cb0740120dc4de8badef60
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-14 22:44:45 +0530

    14-06-2023

--- SuperkeyWord.py
+++ SuperkeyWord.py
@@ -4,12 +4,12 @@ class ParentClass:
 
     def parent_method(self):
         print("This is a parent method")
-
-
+        
 class ChildClass(ParentClass):
 
     def child_method(self):
         print("This is a child method")
+        super().parent_method()
 
 obj = ChildClass()
 obj.child_method()
