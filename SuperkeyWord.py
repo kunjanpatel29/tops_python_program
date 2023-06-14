a56feb3b45c9aaae5e6fbfcf226e5f84206ca966
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-14 22:51:56 +0530

    14-06-2023

--- SuperkeyWord.py
+++ SuperkeyWord.py
@@ -7,9 +7,14 @@ class ParentClass:
         
 class ChildClass(ParentClass):
 
+    def parent_method(self):
+        print("Super Keyword Example")
+        super().parent_method()
+        
     def child_method(self):
         print("This is a child method")
         super().parent_method()
 
 obj = ChildClass()
 obj.child_method()
+obj.parent_method()
