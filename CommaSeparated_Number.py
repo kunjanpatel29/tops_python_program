0e52b664001e73221794d262c4f124cf624bdcb3
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-15 23:04:30 +0530

    15-06-2023

--- CommaSeparated_Number.py
+++ CommaSeparated_Number.py
@@ -1,5 +1,10 @@
 # CommaSeprated numbers from the user and generate list and tuple of numbers.
 
+# List
 lst = input().split(",")
 #lst = list(map(str,input().split(",")))
 print(lst)
+
+# Tuple
+tup = tuple(lst)
+print(tup)
