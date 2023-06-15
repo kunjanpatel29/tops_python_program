c3ea51650e059406a246de4fd56e0e239cabbc21
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-15 23:24:02 +0530

    15-06-2023

--- Generate_Random_Integer.py
+++ Generate_Random_Integer.py
@@ -1,6 +1,7 @@
 # Write Python Program to generate random integer.
 
 import random
+import datetime
 
 print("Generate a random integer between 0 and 6:")
 print(random.randrange(5))
@@ -10,5 +11,11 @@ print(random.randrange(start=5, stop=10))
 
 print("Generate random integer between 0 and 10, with a step of 3:")
 print(random.randrange(start=0, stop=10, step=3))
+
+print("\nRandom date between two dates:")
+start_dt = datetime.date(2023, 5, 1)
+end_dt = datetime.date(2023, 6, 1)
+time_between_dates = end_dt - start_dt
+days_between_dates = time_between_dates.days
 
 
