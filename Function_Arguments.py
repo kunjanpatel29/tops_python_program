078c19abd07ffa86b6152780475382f39c24b029
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-16 23:16:11 +0530

    16-06-2023

--- Function_Arguments.py
+++ Function_Arguments.py
@@ -11,3 +11,11 @@ def my_function(fname, lname):
 
 my_function("Kunjan", "Patel")
 fun("Sanajy")
+
+
+# Keyword Arguments : You can send arguments with the key = value syntax. Order does not matter
+def student_detail(name,rollno,department):
+    print("Name is : ",name)
+
+student_detail(department="IT",name="Kunjan",rollno=29)
+
