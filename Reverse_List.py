ab7608a3da0f25a13666e5d3c5facfa244b0a12c
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-17 23:01:39 +0530

    17-06-2023

---
+++ Reverse_List.py
@@ -0,0 +1,9 @@
+# Write a Program to Print list in reverse order using a loop.
+
+my_list = [10, 20, 30, 40, 50]
+
+new_list = reversed(my_list)  # Reverse List
+
+# iterate reversed list
+for i in new_list:
+    print(i)
