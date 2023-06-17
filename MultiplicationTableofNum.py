8a93638bcf0e5699a0b789fbc79df30cd5a5621c
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-17 22:54:02 +0530

    17-06-2023

---
+++ MultiplicationTableofNum.py
@@ -0,0 +1,9 @@
+# Write a program to print multiplication table of a given number.
+
+num = int(input("Enter a Number : "))
+
+# run loop 10 times
+for i in range(1, 11):
+    product = num * i  # 2*i
+    print(product)
+
