738039f5d6af7519b3d833c73699edcb47518bff
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-19 22:39:48 +0530

    19-06-2023

---
+++ AnagramWords.py
@@ -0,0 +1,9 @@
+# Python Program for find anagram words
+
+str1 = input("Enter the First String  = ")
+str2 = input("Enter the Second String = ")
+
+if(sorted(str1) == sorted(str2)):
+    print("Two Strings are Anagrams.")
+else:
+    print("Two Strings are not Anagrams.")
