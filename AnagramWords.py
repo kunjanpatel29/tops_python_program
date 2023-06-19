9cd3c92c8853b34645c163d2e07c79916588128c
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-19 22:42:22 +0530

    19-06-2023

--- AnagramWords.py
+++ AnagramWords.py
@@ -1,9 +1,21 @@
 # Python Program for find anagram words
 
+# Using Sorted
 str1 = input("Enter the First String  = ")
 str2 = input("Enter the Second String = ")
 
 if(sorted(str1) == sorted(str2)):
+    print("Two Strings are Anagrams.")
+else:
+    print("Two Strings are not Anagrams.")
+
+# Using Counter
+from collections import Counter
+
+str1 = input("Enter the First String  = ")
+str2 = input("Enter the Second String = ")
+
+if(Counter(str1) == Counter(str2)):
     print("Two Strings are Anagrams.")
 else:
     print("Two Strings are not Anagrams.")
