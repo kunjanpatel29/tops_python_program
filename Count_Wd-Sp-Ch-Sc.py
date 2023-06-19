76e9d9dfa55360651cff1396bbbac4d04e6a2997
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-19 23:15:04 +0530

    19-06-2023

--- Count_Wd-Sp-Ch-Sc.py
+++ Count_Wd-Sp-Ch-Sc.py
@@ -1,10 +1,26 @@
 # Count Word,Special Character,Character & Space Program
 
-my_str = int("Enter a String : ")
+my_str = input("Enter a String : ")
 
-wd = 0
+wd = 1
 ch = 0
 sp = 0
 dt = 0
 sc = 0
 
+for i in my_str:
+    if i.isalpha():
+        ch = ch + 1
+    elif i.isspace():
+        sp = sp + 1
+        wd = wd + 1
+    elif i.isnumeric():
+        dt = dt + 1
+    else:
+        sc = sc + 1
+
+print("Total Words is : ",wd)
+print("Total Character is : ",ch)
+print("Total Space is : ",sp)
+print("Total Digit: ",dt)
+print("Total Special Character is : ",sc)
