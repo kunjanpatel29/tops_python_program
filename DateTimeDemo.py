7fa76f41c6eec0880b3f1aa175edd2135b60af08
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-20 23:08:30 +0530

    20-06-2023

--- DateTimeDemo.py
+++ DateTimeDemo.py
@@ -4,3 +4,14 @@ import datetime
 now = datetime.datetime.now()
 
 print(now)
+
+# Get Current Date
+import datetime
+c_date = datetime.date.today()
+print(c_date)
+
+
+# Python datetime.date Class
+import datetime
+c_date = datetime.date(2023,6,20)
+print(c_date)
