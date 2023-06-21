d12388b52cb072a8074b75e2a8e573a9c3d78e87
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-21 22:35:37 +0530

    21-06-2023

--- DateOperations.py
+++ DateOperations.py
@@ -4,3 +4,11 @@ from datetime import datetime
 now = datetime.now()
 
 print(now)
+
+# First Format
+date = now.strftime("%d/%m/%y")
+print(date)
+
+# Second Format
+date = now.strftime("%m/%d/%y")
+print(date)
