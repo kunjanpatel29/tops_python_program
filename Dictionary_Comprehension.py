2077faf6345e14d98f039ff1f7413a5378c07452
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-22 22:32:19 +0530

    22-06-2023

--- Dictionary_Comprehension.py
+++ Dictionary_Comprehension.py
@@ -9,3 +9,12 @@ values = [1,2,3,4,5]
 
 my_dict = {k:v for (k,v) in zip(keys,values)}
 print(my_dict)
+
+#Using fromkeys() Method
+
+d = dict.fromkeys(range(5),True)
+print(d)
+
+# Using dictionary comprehension make dictionary
+dic = {x:x**2 for x in range(1,6)}
+print(dic)
