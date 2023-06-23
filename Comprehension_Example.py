f221cd4e254629b747aa4ff5ebe7f459335a0ad0
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-23 23:11:09 +0530

    23-06-2023

--- Comprehension_Example.py
+++ Comprehension_Example.py
@@ -7,8 +7,6 @@ result = [x for x in lst if x % 2 == 0]
 print(result)
 
 # Example 2
-lst = [1,5,15,18,25,22,14,45,92]
-
 result = [x**3 for x in range(10)]
 
 print(result)
@@ -28,3 +26,16 @@ age = {23,25,24,22}
 d = {key:value for key,value in zip(name,age)}
 
 print(d)
+
+# Set Comprehension
+# Example 1
+s = [1,5,15,18,25,22,14,45,92]
+
+result = {x for x in s if x % 2 == 0}
+
+print(result)
+
+# Example 2
+result = {x**3 for x in range(10)}
+
+print(result)
