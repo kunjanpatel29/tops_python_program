60de77e41b03541b9463942ec61bd7fa38bfd17d
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-23 22:56:40 +0530

    23-06-2023

--- Comprehension_Example.py
+++ Comprehension_Example.py
@@ -12,3 +12,13 @@ lst = [1,5,15,18,25,22,14,45,92]
 result = [x**3 for x in range(10)]
 
 print(result)
+
+# Dictionary Comprehension
+# Example 1
+lst = [1,2,5,9,8,6,4,3]
+
+result = {x:x ** 3 for x in lst if x % 2 != 0}
+
+print(result)
+
+# Example 2
