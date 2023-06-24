28739e83b4fc271ceec76c8fc4f70b1de0e19955
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-24 22:49:36 +0530

    24-06-2023

--- Find_Length_of_list.py
+++ Find_Length_of_list.py
@@ -3,3 +3,13 @@ lst = [10,20,30,40,50,60]
 n = len(lst)
 print("The length of the list is: ", n)
 
+
+# Using count
+lst = [10,20,30,40,50,60]
+
+count = 0
+for i in lst:
+
+    count = count + 1
+
+print("The length of the list is: ", count)
