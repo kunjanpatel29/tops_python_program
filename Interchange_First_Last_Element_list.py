4864dff5040b67a207da7450438377f40e206c71
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-24 22:42:11 +0530

    24-06-2023

---
+++ Interchange_First_Last_Element_list.py
@@ -0,0 +1,15 @@
+# Python program to swap first and last element of a list
+
+# Swap function
+def SwapList(lst):
+	size = len(lst)
+	
+	# Swapping
+	temp = lst[0]
+	lst[0] = lst[size - 1]
+	lst[size - 1] = temp
+	
+	return lst
+	
+lst = [12, 35, 9, 56, 24]
+print(SwapList(lst))
