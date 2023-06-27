1a57e6cb103e2b1ef07d25910199bf112671d153
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-27 22:48:41 +0530

    27-06-2023

---
+++ ListComprehension_odd_even.py
@@ -0,0 +1,17 @@
+lst = [10,25,36,29,45,56,98,92,5]
+
+even_odd = ["Even" if num%2 == 0 else "Odd" for num in lst]
+
+print(even_odd)
+
+
+"""
+even_odd = []
+for i in lst:
+    if i% 2 == 0:
+        even_odd.append("Even")
+    else:
+        even_odd.append("Odd")
+
+print(even_odd)
+"""
