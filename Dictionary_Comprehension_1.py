75b7cb097e452281c029135c689b9a2a8f40cccd
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-27 22:20:33 +0530

    27-06-2023

--- Dictionary_Comprehension_1.py
+++ Dictionary_Comprehension_1.py
@@ -5,3 +5,14 @@ l2 = [1,2,3,4,5,6]
 
 my_dict = {k:v for k,v in zip(l1,l2)} # Using Zip Function
 print(my_dict)
+
+# Without using zip
+my_dict = {l1[i]:l2[i] for i in range(len(l1))} 
+print(my_dict)
+
+"""
+my_dict = {}
+for i in range(len(l1)):
+    my_dict[l1[i]] = l2[i]
+print(my_dict)
+"""
