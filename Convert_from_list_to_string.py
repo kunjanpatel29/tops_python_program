8f91413ae5fa5d0e609d2f0d47085f8655bac5d2
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-28 22:54:36 +0530

    28-06-2023

--- Convert_from_list_to_string.py
+++ Convert_from_list_to_string.py
@@ -1,5 +1,12 @@
 my_list = ["Hello","My","Name","Is","Kunjan","Patel"]
 print(my_list)
 
+print(" ".join(my_list)) # Convert list to string use join function takes only string element
+
 for i in my_list:
     print(i)
+
+lst = [1,2,3,4,5]
+
+a = " ".join(str(lst))
+print(a)
