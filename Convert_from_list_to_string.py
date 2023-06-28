330cbfdf037b5ede07664ab3e1a911c0ad853f07
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-06-28 23:15:24 +0530

    28-06-2023

--- Convert_from_list_to_string.py
+++ Convert_from_list_to_string.py
@@ -10,3 +10,20 @@ lst = [1,2,3,4,5]
 
 a = " ".join(str(i) for i in lst)
 print(a)
+
+# Traversal of list function
+my_list = ["Hello","My","Name","Is","Kunjan","Patel"]
+
+string = ' '
+
+for x in my_list:
+    string += ' '+x
+
+print(string)
+
+# Using map
+my_list = ["Hello","My","Name","Is","Kunjan","Patel",7,8,9]
+
+string = ' '.join(map(str,my_list))
+
+print(string)
