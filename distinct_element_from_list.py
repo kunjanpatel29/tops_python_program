380b9b45bf14380c1bd6328d0621a8b2592de96c
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-07-03 23:10:28 +0530

    03-07-2023

--- distinct_element_from_list.py
+++ distinct_element_from_list.py
@@ -6,10 +6,15 @@ lst = [1,2,3,4,2,2,2,1,5]
 lst1 = []
 
 def distinct_list(lst):
-for i in lst:
-    if i not in lst1:
-            lst1.append(i)
+    for i in lst:
+        if i not in lst1:
+                lst1.append(i)
     return lst1
 
-    
-    
+result = distinct_list(lst)
+print(result)
+
+# Using Set
+lst = [1,2,3,4,2,2,2,1,5]
+lst1 = list(set(lst))
+print(lst1)
