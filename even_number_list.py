d635abfeb7b9d19c4429397d5f74def945fb2244
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-07-03 23:19:06 +0530

    03-07-2023

--- even_number_list.py
+++ even_number_list.py
@@ -3,3 +3,15 @@ Write a Python program to print the even numbers from a given list.
 Sample List : [1, 2, 3, 4, 5, 6, 7, 8, 9]
 Expected Result : [2, 4, 6, 8]
 """
+
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+lst1 = []
+
+def even_num_from_list(lst):
+    for i in lst:
+        if i % 2 == 0:
+            lst1.append(i)
+    return lst1
+
+result = even_num_from_list(lst)
+print(result)
