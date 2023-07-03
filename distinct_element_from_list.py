e0f6c4d7b92a2e7c1218254871a96cb040207f21
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-07-03 23:13:53 +0530

    03-07-2023

--- distinct_element_from_list.py
+++ distinct_element_from_list.py
@@ -1,5 +1,7 @@
 """
-Write a Python function to check whether a number falls within a given range.
+Write a Python function that takes a list and returns a new list with distinct elements from the first list.
+Sample List : [1,2,3,3,3,3,4,5]
+Unique List : [1, 2, 3, 4, 5]
 """
 
 lst = [1,2,3,4,2,2,2,1,5]
