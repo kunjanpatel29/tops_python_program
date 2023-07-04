b569b0a37ed3031ed47b9d948d904527c1ea7d5c
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-07-04 22:46:44 +0530

    04-07-2023

--- RegExDemo.py
+++ RegExDemo.py
@@ -17,3 +17,18 @@ print(b)
 
 b = re.split("\s",txt,1)  # Maxsplit parameter is last
 print(b)
+
+# Sub Method
+import re
+
+# Replace All Whitspace character with the digit 9
+x = re.sub("\s","9",txt)  
+print(x)
+
+# Replace first two occurrence of a white space character with digit 9
+x = re.sub("\s","9",txt,2) 
+print(x)
+
+# Findall Method
+x = re.findall("an",txt)
+print(x)
