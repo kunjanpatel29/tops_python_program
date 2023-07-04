49620f9cb760131af19055da9aa445cc7c0eafe0
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-07-04 22:40:42 +0530

    04-07-2023

--- RegExDemo.py
+++ RegExDemo.py
@@ -7,3 +7,13 @@ txt = "My Name is Kunjan Patel"
 x = re.search("\s",txt)  # Search for first whitespace character in the string
 print("The First Whitespace character in location is : ",x.start())
 
+x = re.search("pqr",txt) # Returns the none if no match
+print(x)
+
+  
+# Split Method
+b = re.split("\s",txt)
+print(b)
+
+b = re.split("\s",txt,1)  # Maxsplit parameter is last
+print(b)
