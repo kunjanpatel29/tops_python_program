7a2cb58cf49c62d23113235c275f318696ccd168
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-07-04 22:33:17 +0530

    04-07-2023

--- check_valid_string_identifier.py
+++ check_valid_string_identifier.py
@@ -5,3 +5,9 @@ Write a Python Program check if string is a valid identifier or not.
 import re
 
 mystr = input("Enter a String : ")
+regex = '^[A-Za-z_][A-Za-z0-9_]*'
+
+if re.search(regex,mystr):
+    print("Valid Identifier")
+else:
+    print("Invalid Identifier")
