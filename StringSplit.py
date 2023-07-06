1e61bb880f009a4043b47b8e0868464e654395a9
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-07-06 23:53:43 +0530

    06-07-2023

--- StringSplit.py
+++ StringSplit.py
@@ -6,3 +6,12 @@ words = mystr.split()
 print(words)
 
 
+# The len() method will give you the number of characters and number of words
+print(len(words))
+print(len(mystr))
+
+
+# String to characters
+word = "Hello"
+result = list(word)
+print(result)
