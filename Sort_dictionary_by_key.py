49a359462fea338d83f396dad8d5f3a0cfbc9875
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-07-06 23:41:51 +0530

    06-07-2023

--- Sort_dictionary_by_key.py
+++ Sort_dictionary_by_key.py
@@ -4,4 +4,7 @@ my_dict = {'kunjan': 10, 'ashish': 9,
 		'isha': 15, 'hanee': 2, 'suraj': 32}
 
 mykeys = list(my_dict.keys())
-print(mykeys)
+mykeys.sort()
+sorted_dict = {i: my_dict[i] for i in mykeys}
+
+print(sorted_dict)
