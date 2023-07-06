8a06527c38afa46bca304442674dc6edf316d37f
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-07-06 23:33:46 +0530

    06-07-2023

--- Merge_Two_Dictionary.py
+++ Merge_Two_Dictionary.py
@@ -1,7 +1,16 @@
 # Merge two Python dictionaries into one
 
+# Using ** 
 d1 = {'one': 1, 'Two': 2, 'Three': 3}
 d2 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
 
 d3 = {**d1,**d2}
+print(d3)
+
+# Using Copy and Update Method
+d1 = {'one': 1, 'Two': 2, 'Three': 3}
+d2 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+
+d3 = d1.copy()
+d3.update(d2)
 print(d3)
