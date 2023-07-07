8b254e40266347c799505efda79434ce1bc33ac2
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-07-07 22:32:35 +0530

    07-07-2023

--- Remove_And_Add_Item_List.py
+++ Remove_And_Add_Item_List.py
@@ -8,3 +8,8 @@ print("Original list ", my_list)
 my_list.pop(4)
 print("List After removing element at index 4 ", my_list)
 
+my_list.insert(2, 15)
+print("List after Adding element at index 2 ", my_list)
+
+my_list.append(29)
+print("List after Adding element at last ", my_list)
