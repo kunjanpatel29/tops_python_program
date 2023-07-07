cb826bdbedac84c485cf2e25bbe1ef8e43f857d1
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-07-07 22:27:58 +0530

    07-07-2023

---
+++ Remove_duplicate_fro_list_find_max_min.py
@@ -0,0 +1,11 @@
+"""
+Write a Python Program for Remove duplicates from a list and create a tuple and find the minimum and maximum number.
+"""
+
+my_list = [87, 52, 44, 53, 54, 87, 52, 53]
+print("Original list", my_list)
+
+my_list = list(set(my_list))
+print("unique list", my_list)
+
+
