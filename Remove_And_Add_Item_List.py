fbe26c487fbf4435476f54774e8f12a58a8566ea
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-07-07 22:31:42 +0530

    07-07-2023

---
+++ Remove_And_Add_Item_List.py
@@ -0,0 +1,10 @@
+"""
+Write a Python Program For Remove and add item in a list
+"""
+
+my_list = [34, 54, 67, 89, 11, 43, 94]
+print("Original list ", my_list)
+
+my_list.pop(4)
+print("List After removing element at index 4 ", my_list)
+
