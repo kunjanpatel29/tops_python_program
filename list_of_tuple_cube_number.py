b641ef1356260e7e1faa670904cc7f4edc1088b7
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-07-08 22:18:06 +0530

    08-07-2023

--- list_of_tuple_cube_number.py
+++ list_of_tuple_cube_number.py
@@ -4,6 +4,11 @@ Write a Python program to create a list of tuples from given list having number 
 
 lst = [2,3,4,6,8,9]
 
+# Using Pow Function
 result = [(value, pow(value,3)) for value in lst]
 
+# Using ** Operator
+result1 = [(value, value ** 3) for value in lst]
+
 print(result)
+print(result1)
