b55be2deb3addff46e0e89fa9cbaba488e674caa
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-07-09 22:54:17 +0530

    09-07-2023

--- Reverse_Dictionary_Mapping.py
+++ Reverse_Dictionary_Mapping.py
@@ -1,6 +1,8 @@
 # Reverse Dictionary Mapping
 
-
 my_dict = {'a':10,'b':20,'c':30,'d':40,'e':50}
+print(my_dict)  # Print Dictionary
 
-print(my_dict)
+# Reverse Mapping
+result_dict = {value: key for key,value in my_dict.items()}
+print(result_dict)
