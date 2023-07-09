cc850d82722600b2d5c63ede3995f29af4165ee7
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-07-09 23:04:11 +0530

    09-07-2023

--- Display_Duplicate_Items_list.py
+++ Display_Duplicate_Items_list.py
@@ -1,9 +1,14 @@
 # Display All Duplicate Items from a List
 
-my_list = [10,20,30,20,40,50,10,20,60,70,80,30]
+my_list = [10,20,30,20,40,10,60,70,80,30]
 exist = {}
 duplicates = []
 
 for x in my_list:
-    print(x)
+    if x not in exist:
+        exist[x] = 1
+    else:
+        duplicates.append(x)
+
+print(duplicates)
 
