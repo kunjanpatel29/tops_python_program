ea974ddcc3cc789d792d9c0a94e1d7af59d6cee2
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-07-10 23:50:03 +0530

    10-07-2023

--- Convert_Two_List_Into_Dictionary.py
+++ Convert_Two_List_Into_Dictionary.py
@@ -8,3 +8,14 @@ values = [1, 4, 5]
 # Printing original keys-value lists
 print("Original key list is : " + str(keys))
 print("Original value list is : " + str(values))
+
+# to convert lists to dictionary
+result = {}
+for key in keys:
+	for value in values:
+		result[key] = value
+		values.remove(value)
+		break
+
+# Printing resultant dictionary
+print("Resultant dictionary is : " + str(result))
