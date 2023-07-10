d6f73e5cc40b8df4ab133fa8c2f621ecc1d1cc9e
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-07-10 23:49:33 +0530

    10-07-2023

---
+++ Convert_Two_List_Into_Dictionary.py
@@ -0,0 +1,10 @@
+# Ways to Convert two lists into a dictionary
+
+
+# initializing lists
+keys = ["abc", "pqr", "bcd"]
+values = [1, 4, 5]
+
+# Printing original keys-value lists
+print("Original key list is : " + str(keys))
+print("Original value list is : " + str(values))
