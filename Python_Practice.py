e9991b7cd3b6ad2e5f40f0f117825fd4fb7721d1
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-07-15 22:27:05 +0530

    15-07-2023

--- Python_Practice.py
+++ Python_Practice.py
@@ -100,3 +100,11 @@ for i in range(1,num+1):
 	for j in range(1,i+1):
 		print(j,end="")
 	print()
+	
+# Another Number Pattern	
+num = int(input("Enter the Number of rows : "))
+for i in range(num,0,-1):
+	for j in range(1,i+1):
+		print(j,end="")
+	print()
+
