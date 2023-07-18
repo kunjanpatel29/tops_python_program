1f084e6706d7d6fd121d3fe5d5b63ce9b96a8b8c
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-07-18 23:04:09 +0530

    18-07-2023

--- practice_python.py
+++ practice_python.py
@@ -5,8 +5,14 @@ result = mystr.strip()
 print(result)
 
 
+# Remove Character from a string in Python
 str1 = 'geeks for geeks'
 print(str1)
  
 str2 = 'ekgs'
 print(str1.strip(str2))
+
+# Remove New line from String in Python
+string = "\nHello, World!\n"
+new_string = string.strip()
+print(new_string)
